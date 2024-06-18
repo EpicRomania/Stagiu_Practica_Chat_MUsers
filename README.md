53a0790457a2bb20281cf4a21d055ab672c07578
EpicRomania <4epicukandromania@gmail.com>
2024-06-18 17:08:54 +0300

    M am documentat despre librariile boost/asio/ssl + despre BD4S pentru libraria SQLiteCpp pentru crearea unei baze de date pentru utilizatori si grupurile cu log create

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Stagiu_Practica_Chat_MUsers
+
+
+# 18.06.2024 
+Am adaugat componentele pentru server si pentru client goale + m am documentat despre libariile boost/asio/ssl pentru a le folosi in proiect
+M am documentat despre SQLiteCpp si BD4S pentru crearea unei baze de date care sa fie folosita pentru stocarea utilizatorilor si
+a grupurilor cu log create de utilizatori
---
+++ SERVER/server.hpp
@@ -0,0 +1,11 @@
+
+
+#include "group_basic.hpp"  /// grup basic , include log uri , nu se sterge niciodata
+#include "group_no_log.hpp" /// la fel ca cel anterior , doar ca nu tine log uri , chat ul e dinaminc , se sterge fie la restartarea servereului
+                            /// fie la cererea ownerului sau a adminilor ( adminii pot doar sa goleasca chat ul , nu si sa l stearga )
+
+class server
+{
+public:
+private:
+};
